ff2588f7e952e4e4f5fd53ba7b598c7e650568e6
vagdur <vilhelm.agdur@gmail.com>
2017-05-29 22:44:23 +0200

    Added support for using masks with the cleanImage and destripeImage functions

--- applyImMask.m
+++ applyImMask.m
@@ -1,2 +1,8 @@
-function imout=applyImMask(im,mask)
+function imout=applyImMask(im,mask,smoothening)
+    if nargin < 2
+        smoothening = 20;
+    end
+    if smoothening
+        mask = imgaussfilt(mask,smoothening);
+    end
     imout = im.*mask + (1-mask)*mean(im(:));
